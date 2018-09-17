1869aa389bb9ae66752be2491e6c90e5eb887e08
zhu733756 <1079333812@qq.com>
2018-09-17 15:26:56 +0800

    update tag v1.2

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -4,3 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("欢迎光临，mysite！")
+
+def add(request,a,b):
+    href='%s/%s'%(a,b)
+    return render(request,"add.html")
+
+def home(request):
+    string="zhuhan"
+    empty_list=[]
+    learn_list=["HTML","CSS","jQuery","Python","Django"]
+    dict_info={"site":"mysite","content":"各种装逼技术"}
+    return render(request,'home.html',{"string":string,
+                                       'empty_list':empty_list,
+                                       "learn_list":learn_list,
+                                       "dict_info":dict_info})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from learn import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.index)
+    path('admin/', admin.site.urls,name="main_views"),
+    path('',views.index,name="index"),
+    path('home/',views.home,name="home"),
+    re_path('^add/(\d+)/(\d+)/$',views.add,name="add")
 ]
